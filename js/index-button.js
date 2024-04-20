d598e87ec21f34f57d5092617618a6ba40d88de6
dduuukk <bender.c@northeastern.edu>
2024-04-19 22:21:18 -0400

    Initial push to check if hosting works here

---
+++ js/index-button.js
@@ -0,0 +1,17 @@
+document.querySelector('.btn-custom').addEventListener('click', function() {
+    // print a message to the console
+    // console.log('Button clicked!');
+    var overlay = document.querySelector('.button-overlay');
+    overlay.classList.add('show');
+
+        // // Remove the 'show' class after a delay
+        // setTimeout(function() {
+        //     overlay.classList.remove('show');
+        // }, 5000);  // Change this to the desired duration
+
+        // Redirect to welcome.html after 1 second
+        setTimeout(function() {
+            window.location.href = "welcome.html";
+            // overlay.classList.remove('show');
+        }, 700);
+});
