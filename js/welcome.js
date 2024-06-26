db64a823418fb97a7176842009911f04ce848607
dduuukk <bender.c@northeastern.edu>
2024-06-26 13:58:38 -0700

    Welcome page titles finished minus flicker #7

--- js/welcome.js
+++ js/welcome.js
@@ -3,6 +3,8 @@
 // Get the button element
 var button = document.querySelector('.hamburger');
 
+
+
 // Add a click event listener
 button.addEventListener('click', function() {
   // Toggle the 'is-active' class
@@ -13,3 +15,18 @@ button.addEventListener('click', function() {
   menu.classList.toggle('show-menu');
   document.body.classList.toggle('blur');
 });
+
+var navbar = document.querySelector('.navbar');
+var navbarHeight = navbar.offsetHeight;
+
+var navbar = document.querySelector('.navbar');
+var navbarHeight = navbar.offsetHeight;
+
+var title = document.querySelector('.animated-title');
+title.style.top = `calc(50% - ${navbarHeight}px)`;
+
+var subtitle = document.querySelector('.animated-subtitle');
+subtitle.style.top = `calc(50% - ${navbarHeight}px)`;
+
+var line = document.querySelector('.line');
+line.style.top = `calc(50% - ${navbarHeight}px)`;
